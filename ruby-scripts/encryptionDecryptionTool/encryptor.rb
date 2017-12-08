60cb040d15e6f5abf32a6e842bd0322d32c973d0
= <=>
2017-12-08 11:11:45 +0100

    Encryption Decryption Tool

---
+++ ruby-scripts/encryptionDecryptionTool/encryptor.rb
@@ -0,0 +1,77 @@
+class Encryptor
+	def cipher(rotation) #this is our reference for encryption
+
+		characters = (' '..'z').to_a
+		rotatedCharacters = characters.rotate(rotation)
+		pair = characters.zip(rotatedCharacters)
+		Hash[pair]
+
+	end
+
+	def encrypt_letter(letter, rotation)
+
+		cipher(rotation)[letter]
+
+	end
+
+	def decrypt_letter(letter, rotation)
+
+		cipher_rotated = cipher(rotation)
+		inverted_cipher = cipher_rotated.invert
+		decrypted_cipher = inverted_cipher[letter]
+	end
+
+	def encrypt(string, rotation)
+
+		letters = string.split("")
+		result = letters.map{ |letter| encrypted_letters = encrypt_letter(letter, rotation)	}
+
+		result.join
+
+	end
+
+	def decrypt(string, rotation)
+		letters = string.split("")
+		result = letters.map{|letter| decrypted_letters = decrypt_letter(letter, rotation)}
+
+		result.join
+	end
+
+	def encrypt_file(filename, rotation)
+		the_file = File.open(filename, "r")
+		passage = the_file.read
+		encrypted_passage = encrypt(passage, rotation)
+		the_file.close
+		output = File.open("encryptedFile.txt", "w")
+		output.write(encrypted_passage)
+		output.close
+	end
+
+	def decrypt_file(filename, rotation)
+		input = File.open(filename, "r")
+		passage = input.read
+		decrypted_passage = decrypt(passage, rotation)
+		input.close
+		output = File.open("decryptedFile.txt", "w")
+		output.write(decrypted_passage)
+		output.close
+	end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
